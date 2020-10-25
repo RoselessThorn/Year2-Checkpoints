e0e6cd508873d67f4af14a71556c9bd44f392011
RoselessThorn <38583455+RoselessThorn@users.noreply.github.com>
2020-10-25 13:31:41 +0000

    Update checkpoint1.py

--- checkpoint1.py
+++ checkpoint1.py
@@ -4,7 +4,13 @@ Created on Tue Sep 29 16:13:16 2020
 
 @author: HP
 """
+#Write a Python program to calculate and print to the terminal the radius and surface area of a sphere from its volume.
 
+#Your program should:
+
+#Prompt for the volume of a sphere in mm3.
+#Calculate and print out to the terminal the radius and the surface area of the sphere in m and m2 respectively in an understandable format.
+#Check your program works for a range of volumes, both large and small and that you have the output units correct.
 import math
 v=input("PLease enter the volumn in mm^3：") #input the volumn as string
 v=float(v)                                  #change the string into float
