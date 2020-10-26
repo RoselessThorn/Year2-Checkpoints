374c2015e4b3aee653355aeb62e87e78eb096a25
RoselessThorn <38583455+RoselessThorn@users.noreply.github.com>
2020-10-26 16:02:18 +0000

    Add files via upload

--- checkpoint4.py
+++ checkpoint4.py
@@ -4,15 +4,7 @@ Created on Sat Oct 24 19:49:39 2020
 
 @author: HP
 """
-#The computing task is to write a Python program to read in data from the supplied file for voltage ( V(t)) and current ( I(t)) and plot, via pyplot, the log power, being,
-#p(t)=log(V(t)I(t))
-#where log is natural log to base e.
 
-#Your program should:
-
-#Prompt for the filename to be read.
-#Read in the required data from sample.txt file and display p(t) over the supplied range of t with suitable titles and axis labels.
-#You must use a function of the form logpower(voltage,current) to form the p(t), and a main() program.
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,7 +14,7 @@ def main():                         #main function
     with open(path,'r') as f:       #input each element into the list divided by coma
         for line in f:
             result.append(list(line.strip('\n').split(',')))
-    #print(len(result))
+    print(len(result))
     #print(result)
     for i in range(len(result)):    #to determine from which element the comment stops
         f = str((result[i])[0]).startswith('#')
@@ -36,16 +28,22 @@ def main():                         #main function
         voltage.append((result[o])[0])
         current.append((result[o])[1])
     #print(voltage)
-    p = []              #set up a empty list for P(t)
+    p = []                      #set up a empty list for P(t)
     #print((len(result)-i))
-    for k in range((len(result)-i)):        #each P(t) value is calculated from the corresponding V and I value
-        p.append(math.log(float(voltage[k])*float(current[k])))
-        #print(p)
-        step = 25e3/(len(result)-i)         #step up the corresponding data for t
-        t = np.arange(0.0,25E3,step) 
+    def logpower(voltage,current):
+        d = []              
+        for k in range((len(result)-i)):        #each P(t) value is calculated from the corresponding V and I value
+            d.append(math.log(float(voltage[k])*float(current[k])))
+        return d
+    p = logpower(voltage,current)       #get all p values in one list
+    time_range = ((len(result)-i)/25)         #set up the corresponding data for t in microseconds
+    step = time_range/(len(result)-i)           #step used to arange the t function
+    t = np.arange(0.0,time_range,step) 
+    #print(time_range)
+    #print(t)
     #print (p)
     plt.plot(t,p)   #plot the diagram with x and y axis with t and p(t)
-    plt.xlabel('t',fontproperties='Kaiti',fontsize=14,color='red')       #labels
+    plt.xlabel('t(in microseconds)',fontproperties='Kaiti',fontsize=14,color='red')       #labels
     plt.ylabel('P(t)',fontproperties='SimHei',fontsize=18,color='red')
     plt.title('p(t)=log(V(t)I(t))',fontproperties='SimHei',fontsize=18,color='green')  #title
     plt.grid(True)              #grid
